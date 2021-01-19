0672b9a90934d9a28a4773075d321ceab235b5b5
jja33705 <jja33705@gmail.com>
2021-01-19 12:11:51 +0900

    :)

--- JavaPractice/src/ch08Practice/MyFrame.java
+++ JavaPractice/src/ch08Practice/MyFrame.java
@@ -1,5 +1,7 @@
 package ch08Practice;
 
+import java.awt.Color;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -12,6 +14,7 @@ public class MyFrame extends JFrame{
 		JPanel panelA=new JPanel();
 		JLabel label=new JLabel("자바 피자에 오신 것을 환영합니다. 피자의 종류를 선택하시오.");
 		panelA.add(label);
+		panelA.setBackground(Color.CYAN);
 		panel.add(panelA);
 		JPanel panelB=new JPanel();
 		JButton button1=new JButton("콤보피자");
@@ -24,7 +27,9 @@ public class MyFrame extends JFrame{
 		panelB.add(count);
 		JTextField textField=new JTextField(10);
 		panelB.add(textField);
+		panelB.setBackground(Color.GREEN);
 		panel.add(panelB);
+		panel.setBackground(Color.BLUE);
 		this.add(panel);
 		this.setTitle("MyFrame");
 		this.setSize(500, 150);
