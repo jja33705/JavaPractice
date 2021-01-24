a51aabd451916e69a0a89f92c61427c8aeee7fac
jja33705 <jja33705@gmail.com>
2021-01-24 23:28:32 +0900

    :-)

--- JavaPractice/src/ch10Practice/Puzzle.java
+++ JavaPractice/src/ch10Practice/Puzzle.java
@@ -52,7 +52,118 @@ public class Puzzle extends JFrame implements ActionListener{
 			return;
 		}
 		if(button.index==0) {
-			
+			if(buttons[1].getText().equals(" ")) {
+				buttons[1].setText(button.getText());
+				button.setText(" ");
+			}
+			if(buttons[3].getText().equals(" ")) {
+				buttons[3].setText(button.getText());
+				button.setText(" ");
+			}
+		}
+		if(button.index==1) {
+			if(buttons[0].getText().equals(" ")) {
+				buttons[0].setText(button.getText());
+				button.setText(" ");
+			}
+			if(buttons[2].getText().equals(" ")) {
+				buttons[2].setText(button.getText());
+				button.setText(" ");
+			}
+			if(buttons[4].getText().equals(" ")) {
+				buttons[4].setText(button.getText());
+				button.setText(" ");
+			}
+		}
+		if(button.index==2) {
+			if(buttons[1].getText().equals(" ")) {
+				buttons[1].setText(button.getText());
+				button.setText(" ");
+			}
+			if(buttons[5].getText().equals(" ")) {
+				buttons[5].setText(button.getText());
+				button.setText(" ");
+			}
+		}
+		if(button.index==3) {
+			if(buttons[0].getText().equals(" ")) {
+				buttons[0].setText(button.getText());
+				button.setText(" ");
+			}
+			if(buttons[4].getText().equals(" ")) {
+				buttons[4].setText(button.getText());
+				button.setText(" ");
+			}
+			if(buttons[6].getText().equals(" ")) {
+				buttons[6].setText(button.getText());
+				button.setText(" ");
+			}
+		}
+		if(button.index==4) {
+			if(buttons[1].getText().equals(" ")) {
+				buttons[1].setText(button.getText());
+				button.setText(" ");
+			}
+			if(buttons[3].getText().equals(" ")) {
+				buttons[3].setText(button.getText());
+				button.setText(" ");
+			}
+			if(buttons[5].getText().equals(" ")) {
+				buttons[5].setText(button.getText());
+				button.setText(" ");
+			}
+			if(buttons[7].getText().equals(" ")) {
+				buttons[7].setText(button.getText());
+				button.setText(" ");
+			}
+		}
+		if(button.index==5) {
+			if(buttons[2].getText().equals(" ")) {
+				buttons[2].setText(button.getText());
+				button.setText(" ");
+			}
+			if(buttons[4].getText().equals(" ")) {
+				buttons[4].setText(button.getText());
+				button.setText(" ");
+			}
+			if(buttons[8].getText().equals(" ")) {
+				buttons[8].setText(button.getText());
+				button.setText(" ");
+			}
+		}
+		if(button.index==6) {
+			if(buttons[3].getText().equals(" ")) {
+				buttons[3].setText(button.getText());
+				button.setText(" ");
+			}
+			if(buttons[7].getText().equals(" ")) {
+				buttons[7].setText(button.getText());
+				button.setText(" ");
+			}
+		}
+		if(button.index==7) {
+			if(buttons[4].getText().equals(" ")) {
+				buttons[4].setText(button.getText());
+				button.setText(" ");
+			}
+			if(buttons[6].getText().equals(" ")) {
+				buttons[6].setText(button.getText());
+				button.setText(" ");
+			}
+			if(buttons[8].getText().equals(" ")) {
+				buttons[8].setText(button.getText());
+				button.setText(" ");
+			}
+		}
+		if(button.index==8) {
+			if(buttons[5].getText().equals(" ")) {
+				buttons[5].setText(button.getText());
+				button.setText(" ");
+			}
+			if(buttons[7].getText().equals(" ")) {
+				buttons[7].setText(button.getText());
+				button.setText(" ");
+			}
 		}
 	}
 
