09b87639c9e290dc78844c44326a6e3b78332f4e
jja33705 <jja33705@gmail.com>
2021-03-18 21:46:22 +0900

    계산기 어렵다.........

--- JavaPractice/src/ch13Practice/Score.java
+++ JavaPractice/src/ch13Practice/Score.java
@@ -15,7 +15,7 @@ public class Score {
 		g2d.setColor(Color.WHITE);
 		g2d.drawLine(435, 0, 435, 500);
 		g2d.setColor(Color.BLACK);
-		g2d.setFont(new Font("Serif",  Font.BOLD, 50));
+		g2d.setFont(new Font("¸¼Àº °íµñ",  Font.CENTER_BASELINE, 50));
 		g2d.drawString(""+game.p1Score/10 + game.p1Score%10, 350, 50);
 		g2d.drawString(""+game.p2Score/10 + game.p2Score%10, 460, 50);
 	}
